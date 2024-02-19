1780c4091df795af132d79936a2283909752a632
OpenSauce04 <opensauce04@gmail.com>
2024-02-19 13:38:01 +0000

    Removed image cache due to Docker's existing cache making it obsolete

--- mouc/__main__.py
+++ mouc/__main__.py
@@ -1,6 +1,5 @@
 import sys
 
-from command.clean import cmd_clean
 from command.enter import cmd_enter
 import strings
 
@@ -15,9 +14,6 @@ match sys.argv[1]:
       print(strings.program_usage)
       exit(1)
     cmd_enter(sys.argv[2])
-
-  case 'clean':
-    cmd_clean()
 
   case _:
     print(strings.program_usage)
--- mouc/command/enter.py
+++ mouc/command/enter.py
@@ -1,4 +1,3 @@
-import hashlib
 from os.path import join as join_path
 from subprocess import run
 
@@ -7,18 +6,10 @@ from utils import *
 
 def cmd_enter(image):
   dockerfile_path = join_path(dockerfiles_dir, image)
-  with open(dockerfile_path, 'rb', buffering=0) as f:
-    dockerfile_hash = hashlib.file_digest(f, 'md5').hexdigest()
-  image_cache_path = join_path(image_cache_dir, dockerfile_hash)
 
   qrun(['docker', 'rmi', '-f', 'mouc-managed'], True)
+  qrun(['docker', 'build', '-t', 'mouc-managed', '-f', dockerfile_path, home_dir])
 
-  if not isfile(image_cache_path):
-    qrun(['docker', 'build', '-t', 'mouc-managed', '-f', dockerfile_path, home_dir])
-    qrun(['docker', 'save', '-o', image_cache_path, 'mouc-managed'])
-    qrun(['docker', 'rmi', '-f', 'mouc-managed'])
-
-  qrun(['docker', 'load', '-q', '-i', image_cache_path])
   qrun(['su', username, '-c', 'xhost local:root'])
   run([
     'docker', 'run', '-it',
@@ -29,4 +20,5 @@ def cmd_enter(image):
       '--workdir', f'/var/host/{os.getcwd()}',
       'mouc-managed', '/bin/bash'
     ])
+
   qrun(['docker', 'rmi', '-f', 'mouc-managed'])
