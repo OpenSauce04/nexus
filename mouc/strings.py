ea7a1a2e16eb5f94b396ec56399aee1490bdffbd
OpenSauce04 <opensauce04@gmail.com>
2024-02-19 13:41:56 +0000

    Removed unused string

--- mouc/strings.py
+++ mouc/strings.py
@@ -8,7 +8,6 @@ username = os.environ.get('SUDO_USER', os.environ.get('USERNAME'))
 home_dir = expanduser(f'~{username}')
 mouc_dir = home_dir+'/.mouc'
 dockerfiles_dir = mouc_dir+'/dockerfiles'
-image_cache_dir = mouc_dir+'/imagecache'
 
 # Messages
 program_usage = "Usage: enter"
